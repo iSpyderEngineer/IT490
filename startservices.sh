e4e3f24e70fb112afbc02a81b6dac67e48f65236
nw77-NicholasWagner <50148007+100billman1@users.noreply.github.com>
2022-02-08 00:36:36 -0500

    added shh connection with proper machines/services

--- startservices.sh
+++ startservices.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
-sudo mysql -u root -p
-sudo service rabbitmq-server start
-sudo service apache2 start
+ssh -t -p 22 "enter rahi's systemname@192.168.194.225" sudo mysql -u root -p
+ssh -t -p 22 it490@192.168.194.26 sudo service rabbitmq-server start
+ssh -t -p 22 md523@192.168.194.175 sudo service apache2 start
