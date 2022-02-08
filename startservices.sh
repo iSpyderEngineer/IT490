828b99b5e043be617ef41c49ec7a9937287b7829
nw77-NicholasWagner <50148007+100billman1@users.noreply.github.com>
2022-02-08 00:45:00 -0500

    Update startservices.sh

--- startservices.sh
+++ startservices.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
-ssh -t -p 22 "enter rahi's systemname@192.168.194.225" sudo mysql -u root -p
+ssh -t -p 22 rahi@192.168.194.225 sudo mysql -u root -p
 ssh -t -p 22 it490@192.168.194.26 sudo service rabbitmq-server start
 ssh -t -p 22 md523@192.168.194.175 sudo service apache2 start
