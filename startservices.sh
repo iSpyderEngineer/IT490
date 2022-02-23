b34ed4dd8240e52207bad1fb54d1cdc6d726bb05
nw77-NicholasWagner <50148007+100billman1@users.noreply.github.com>
2022-02-22 19:04:26 -0500

    updated the ips for each machine

--- startservices.sh
+++ startservices.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
-ssh -t -p 22 rahi@192.168.194.225 sudo mysql -u root -p
-ssh -t -p 22 it490@192.168.194.26 sudo service rabbitmq-server start
-ssh -t -p 22 md523@192.168.194.175 sudo service apache2 start
+ssh -t -p 22 rahi@192.168.194.201 sudo mysql -u root -p
+ssh -t -p 22 it490@192.168.194.33 sudo service rabbitmq-server start
+ssh -t -p 22 md523@192.168.194.253 sudo service apache2 start
