791c8d08d4efb4022019763a8b35efcaa9477520
JayssonDiaz <jsd42@njit.edu>
2023-10-30 22:52:48 -0400

    Added consumer scripts and edited producer script

--- RabbitMQ/userValidate.php
+++ RabbitMQ/userValidate.php
@@ -5,10 +5,15 @@ require_once(__DIR__ . '/server/rabbitMQLib.inc');
 
 if ($_POST){
   $request = array();
-  $request["type"] = $_POST["type"];
-  $request["message"] = $_POST["message"];
+  $password = hash("sha256", $_POST["password"]);
+  $request['type'] = $_POST["type"];
+  $request['username'] = $_POST["username"];
+  $request['password'] = $password;
 
   $client = new rabbitMQClient("testRabbitMQ.ini","database");
+  if (isset($_POST["email"])){
+	  $request["email"] = $_POST["email"];
+  }
   
   $response = $client -> send_request($request);
   echo $response;
