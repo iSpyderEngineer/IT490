3c9c1fb9c30e73d4fe4059b7f7ea5a559a803c91
JayssonDiaz <jsd42@njit.edu>
2023-11-13 21:44:49 -0500

    made changes to rabbit server scripts

--- RabbitMQ/apiServer.php
+++ RabbitMQ/apiServer.php
@@ -10,10 +10,28 @@ function requestProcessor($request){
   echo "received request" . PHP_EOL;
   var_dump($request);
   switch ($request['type']) {
-    case "recommend":
-        return recommend();
+    case "displayRecommendedMovies":
+      echo "Displaying recommended movies";
+      return displayRecommendedMovies($request['movieData'], $request['source']);
+
+    case "searchMovies":
+      echo "Searching for movies";
+      return searchMoviesAndTVShows($request['query']);
+
+    case "fetchUserProfile":
+      echo "Fetching user profile";
+      return fetchUserProfile($request['sessionID']);
+
+    case "updateUserPreferences":
+      echo "Updating user preferences";
+      return updateUserPreferences($request['userID'], $request['preferences']);
+
+    default:
+      echo "Request type not handled";
+      return ["error" => "Request type not supported"];
   }
 }
+
 
 
 $server = new rabbitMQServer("testRabbitMQ.ini","api");
