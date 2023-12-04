b8e9f90cd98679397e682908412019a871f1d1d2
Brendan Powers <snp76@njit.edu>
2023-12-03 20:09:21 -0500

    update api server

--- RabbitMQ/apiServer.php
+++ RabbitMQ/apiServer.php
@@ -3,54 +3,90 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-require_once('../Backend/apiFunctions.inc');
+require_once('../Backend/apiFunctions.inc'); // Ensure correct path
 
 function requestProcessor($request){
   echo "received request" . PHP_EOL;
   var_dump($request);
-  switch ($request['type']) {
-    case "searchMovies":
-      echo "Searching for movies";
-      return searchMoviesAndTVShows($request['query']);
 
+  if (!isset($request['type'])) {
+    return ['status' => 'error', 'message' => 'Request type is missing'];
+  }
+
+  switch ($request['type']) {
     case "searchMoviesAndTVShows":
-      echo "Searching for tv shows and movies";
-      return searchMoviesandTVShows($request['query']);
+      if (isset($request['query'])) {
+        $response = searchMoviesandTVShows($request['query']);
+        var_dump($response);
+        return $response;
+      } else {
+        return ['status' => 'error', 'message' => 'Query parameter is missing'];
+      }
 
     case "searchPerson":
-      echo "Searching for a person";
-      return searchPerson($request['personName']);
+      if (isset($request['personName'])) {
+        $response = searchPerson($request['personName']);
+        var_dump($response);
+        return $response;
+      } else {
+        return ['status' => 'error', 'message' => 'Person name parameter is missing'];
+      }
 
     case "recommendationActorDirector":
-      echo "Getting recommendations based on actor and director";
-      return recommendationActorDirector($request['username']);
+      if (isset($request['username'])) {
+        $response = recommendationActorDirector($request['username']);
+        var_dump($response);
+        return $response;
+      } else {
+        return ['status' => 'error', 'message' => 'Username parameter is missing'];
+      }
 
     case "getMoviesByActor":
-      echo "Getting movies by actor";
-      return getMoviesByActor($request['actorName']);
+      if (isset($request['actorName'])) {
+        $response = getMoviesByActor($request['actorName']);
+        var_dump($response);
+        return $response;
+      } else {
+        return ['status' => 'error', 'message' => 'Actor name parameter is missing'];
+      }
 
     case "getMoviesByDirector":
-      echo "Getting movies by director";
-      return getMoviesByDirector($request['directorName']);
+      if (isset($request['directorName'])) {
+        $response = getMoviesByDirector($request['directorName']);
+        var_dump($response);
+        return $response;
+      } else {
+        return ['status' => 'error', 'message' => 'Director name parameter is missing'];
+      }
 
     case "getMoviesByMovieAndGenre":
-      echo "Getting movies by movie and genre";
-      return getMoviesByMovieAndGenre($request['username']);
+      if (isset($request['username'])) {
+        $response = getMoviesByMovieAndGenre($request['username']);
+        var_dump($response);
+        return $response;
+      } else {
+        return ['status' => 'error', 'message' => 'Username parameter is missing'];
+      }
 
     case "getMoviesByDetails":
-      echo "Getting details for movie";
-      return getMoviesByDetails($request['movieID']);
+      if (isset($request['movieID'])) {
+        $response = getMoviesByDetails($request['movieID']);
+        var_dump($response);
+        return $response;
+      } else {
+        return ['status' => 'error', 'message' => 'Movie ID parameter is missing'];
+      }
 
     default:
-      echo "Request type not handled";
-      return ["error" => "Request type not supported"];
+      echo "Request type not handled\n";
+      return ['status' => 'error', 'message' => "Request type '{$request['type']}' not supported"];
   }
 }
 
 $server = new rabbitMQServer("testRabbitMQ.ini","api");
 
 echo "api server started up" . PHP_EOL;
-$server -> process_requests('requestProcessor');
+$server->process_requests('requestProcessor');
 echo "api server shut down" . PHP_EOL;
 exit();
 ?>
